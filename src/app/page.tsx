106154be0f9d04aea5cb01a29898ea6b9e380ef4
Emanuele Maria <emanuuuelem@gmail.com>
2025-08-22 13:31:20 -0300

    Enhance project section layout and update menu functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <main className={styles.main}>
         <header className={styles.header}>
           <section className={styles.intro}>
-            <ul className={styles.menu}>
+            <ul className={styles.menu} onBlur={() => setIsMenuOpen(false)} tabIndex={0}>
               <li className={styles.menuItem} onClick={() => setIsMenuOpen(!isMenuOpen)}>{isMenuOpen ? <CloseCircleOutlined /> : <MenuOutlined />}</li>
               {isMenuOpen && (
                 <>
@@ -89,41 +89,47 @@ export default function Home() {
           <h2 className={styles.title}>Projetos</h2>
           <article className={styles.container}>
             <div className={styles.projectList}>
-              <a href="https://ttravelling.netlify.app/">
 
-                <div className={styles.projectItem}>
-                  <div className={styles.projectImage}>
-                    <img src="/ttravel.png" alt="" />
-                  </div>
+              <div className={styles.projectItem}>
+                <div className={styles.projectImage}>
+                  <img src="/ttravel.png" alt="" />
+                </div>
+                <div className={styles.projectInfo}>
                   <h2>TTRAVEL</h2>
                   <p>Site que calcula o custo de combustível em viagens, integrando APIs do IBGE e
                     OpenRouteService. O framework utilizado foi o Next.js.</p>
                 </div>
-              </a>
-              <a href="https://front-academia-augusto.vercel.app/">
-                <div className={styles.projectItem}>
-                  <div className={styles.projectImage}>
-                    <img src="/academia.png" alt="" />
-                  </div>
+                <div>ver projeto <a href="https://ttravelling.netlify.app/">aqui</a></div>
+              </div>
+              <div className={styles.projectItem}>
+                <div className={styles.projectImage}>
+                  <img src="/academia.png" alt="" />
+                </div>
+                <div className={styles.projectInfo}>
                   <h2>Ah, cadê mia?</h2>
                   <p>Sistema de gerenciamento de academia desenvolvido com React.js, Node.js e Postgres.</p>
                 </div>
-              </a>
-              <a href="https://blog-3bk5.onrender.com/">
+                <div>ver projeto <a href="https://front-academia-augusto.vercel.app/">aqui</a></div>
+              </div>
                 <div className={styles.projectItem}>
                   <div className={styles.projectImage}>
                     <img src="/blog.png" alt="" />
                   </div>
-                  <h2>Blog</h2>
-                  <p>Projeto completo de blog com painel administrativo, autenticação e CRUD.</p>
+                  <div className={styles.projectInfo}>
+                    <h2>Blog</h2>
+                    <p>Projeto completo de blog com painel administrativo, autenticação e CRUD.</p>
+                  </div>
+                  <div>ver projeto <a href="https://blog-3bk5.onrender.com/">aqui</a></div>
                 </div>
-              </a>
               <div className={styles.projectItem}>
                 <div className={styles.projectImage}>
                   <img src="/portfolio.png" alt="" />
                 </div>
-                <h2><a href="#">Portfólio</a></h2>
-                <p>Este portfólio foi desenvolvido utilizando Next.js e TypeScript, com foco em performance e SEO. Futuramente pretendo adicionar mensageria para contato.</p>
+                <div className={styles.projectInfo}>
+                  <h2><a href="#">Portfólio</a></h2>
+                  <p>Este portfólio foi desenvolvido utilizando Next.js e TypeScript, com foco em performance e SEO. Futuramente pretendo adicionar mensageria para contato.</p>
+                </div>
+                <div><a href="#">você já está aqui 😅 </a></div>
               </div>
             </div>
           </article>
